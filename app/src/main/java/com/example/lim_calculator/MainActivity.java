2b7a2ad216ae2ae5b40828d31265ea9f08fc68e4
PocariNoSweat <christianlim0031@gmail.com>
2022-01-26 02:01:24 +0900

    My calculator works now

--- app/src/main/java/com/example/lim_calculator/MainActivity.java
+++ app/src/main/java/com/example/lim_calculator/MainActivity.java
@@ -8,62 +8,82 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    EditText var1txt, var2txt;
-    Button btnAdd, btnMinus, btnMultiply, btnDivide, btnModulo;
-    TextView answerTxt;
-    double variable1, variable2;
+    EditText editTextVar1,editTextVar2;
+    Button btnAdd,btnMinus,btnMultiply,btnDivide,btnModulo;
+    TextView AnswerTxt;
+
+    double var1,var2;
+
+
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //buttons
+        btnAdd = findViewById(R.id.btnAdd);
+        btnMinus = findViewById(R.id.btnMinus);
+        btnDivide = findViewById(R.id.btnDivide);
+        btnMultiply = findViewById(R.id.btnMultiply);
+        btnModulo = findViewById(R.id.btnModulo);
+
+
+
+        //Answer text view
+        AnswerTxt = findViewById(R.id.AnswerTxt);
+
+        //creating button on-click listeners to make the buttons work
         btnAdd.setOnClickListener(this);
         btnMinus.setOnClickListener(this);
         btnMultiply.setOnClickListener(this);
         btnDivide.setOnClickListener(this);
         btnModulo.setOnClickListener(this);
 
-        //text boxes
-        var1txt = findViewById(R.id.editTextVar1);
-        var2txt = findViewById(R.id.editTextVar2);
-        //buttons
-        btnAdd = findViewById(R.id.btnAdd);
-        btnMinus = findViewById(R.id.btnMinus);
-        btnMultiply = findViewById(R.id.btnMultiply);
-        btnDivide = findViewById(R.id.btnDivide);
-        btnModulo = findViewById(R.id.btnModulo);
-        //Answer
-        answerTxt = findViewById(R.id.AnswerTxt);
-
-        variable1 = Double.parseDouble(String.valueOf(var1txt.getText()));
-        variable2 = Double.parseDouble(String.valueOf(var2txt.getText()));
-
-
-
     }
     @Override
-    public void onClick(View v) {
+    public void onClick(View v) { //this method is responsible for doing functions for on-click events
 
+        //variable text boxes
 
-        switch(v.getId()) {
+        editTextVar1 = findViewById(R.id.editTextVar1);
+        editTextVar2 = findViewById(R.id.editTextVar2);
+
+        var1 = Double.parseDouble(editTextVar1.getText().toString());
+        var2 = Double.parseDouble(editTextVar2.getText().toString());
+
+        switch(v.getId()){
             case R.id.btnAdd:
-                answerTxt.setText(String.valueOf( variable1 + variable2 ));
+                AnswerTxt.setText(Double.toString(var1 + var2));
+
                 break;
             case R.id.btnMinus:
-                answerTxt.setText(String.valueOf( variable1 - variable2 ));
+                AnswerTxt.setText(Double.toString(var1 - var2));
+
                 break;
             case R.id.btnMultiply:
-                answerTxt.setText(String.valueOf(variable1 * variable2));
+                AnswerTxt.setText(Double.toString(var1 * var2));
+
                 break;
             case R.id.btnDivide:
-                answerTxt.setText(String.valueOf(variable1 / variable2));
+                AnswerTxt.setText(Double.toString(var1 / var2));
+
                 break;
             case R.id.btnModulo:
-                answerTxt.setText(String.valueOf(variable1 % variable2));
+                AnswerTxt.setText(Double.toString(var1 % var2));
+
                 break;
         }
+
     }
+
+
 }
